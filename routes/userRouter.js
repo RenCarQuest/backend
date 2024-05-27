f4c75b775ad0700a2c54b32b00424222f0cc5882
JulianaCelis <juliana0celis@gmail.com>
2024-05-27 18:24:58 -0500

    Fix token

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const userRoutes = express.Router();
-const authenticateToken = require('../middlewares/authToken');
+// const authenticateToken = require('../middlewares/authToken');
 const { registerUser, allUsers, loginUser, deleteUser, logoutUser } = require('../controllers/User/userControllers');
 
 userRoutes.post('/register', registerUser);
@@ -8,6 +8,6 @@ userRoutes.post('/login', loginUser);
 userRoutes.post('/logout', logoutUser);
 userRoutes.delete('/delete/:id', deleteUser);
 userRoutes.get('/all', allUsers);
-userRoutes.get('/checkSession', authenticateToken);
+// userRoutes.get('/checkSession', authenticateToken);
 
 module.exports = userRoutes;
