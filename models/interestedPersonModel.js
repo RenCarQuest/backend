47a450923d733c769a186a84ecf14c74ffca0b81
JulianaCelis <juliana0celis@gmail.com>
2024-06-13 13:09:57 -0500

    Modelo interested person

--- models/interestedPersonModel.js
+++ models/interestedPersonModel.js
@@ -1,25 +1,27 @@
-const InterestedPerson = require('../../models/interestedPersonModel');
+const mongoose = require('mongoose');
 
-async function registerInterest(req, res) {
-    try {
-        const { name, email, phone } = req.body;
-
-        if (!email) {
-            return res.status(400).json({ error: 'Email is required' });
-        }
-
-        const newInterest = new InterestedPerson({
-            name,
-            email,
-            phone
-        });
-
-        await newInterest.save();
-        res.status(201).json({ message: 'Interest registered successfully', data: newInterest });
-    } catch (error) {
-        console.error('Failed to register interest:', error);
-        res.status(500).json({ error: 'An error occurred during the registration process' });
+const interestedPersonSchema = new mongoose.Schema({
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    name: {
+        type: String,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        match: [/.+\@.+\..+/, 'Please fill a valid email address']
+    },
+    phone: {
+        type: String,
+        trim: true
     }
-}
+});
 
-module.exports = registerInterest;
+const InterestedPerson = mongoose.model('InterestedPerson', interestedPersonSchema);
+
+module.exports = InterestedPerson;
